b1910b90a6a1a7e7c2cffa6a0bda7f8bcc609e96
Alghuul <amineihsane@gmail.com>
2021-02-02 14:53:08 +0100

    update persistence

--- src/main/java/com/fr/uha/ensisa/java/restapi/model/PassagePoint.java
+++ src/main/java/com/fr/uha/ensisa/java/restapi/model/PassagePoint.java
@@ -1,11 +1,17 @@
 package com.fr.uha.ensisa.java.restapi.model;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
-@Entity
-public class PassagePoint {
 
+
+@Entity
+public class PassagePoint implements Serializable{
+
+	private final static long SerialVersionUID = 1L;
+	
 	@Id
 	private String challengeID;
 	
--- src/main/java/com/fr/uha/ensisa/java/restapi/model/Segment.java
+++ src/main/java/com/fr/uha/ensisa/java/restapi/model/Segment.java
@@ -4,18 +4,18 @@ package com.fr.uha.ensisa.java.restapi.model;
 import java.util.List;
 import java.util.Map;
 
+import javax.persistence.Entity;
 import javax.persistence.Id;
 
+@Entity
 public class Segment {
 
 	
 	@Id
 	private String challengeID;
-	
-	
 	private PassagePoint beginPPoint;
 	private PassagePoint endPPoint;
-	private Map<Obstacle, Integer> obstacles ;
+//	private Map<Obstacle, Integer> obstacles ;
 	private int km;
 
 	
