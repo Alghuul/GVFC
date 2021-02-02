3d17fd76244889d3996723c0e75a00c3f12963f9
nodcM <adam.chaieb@live.fr>
2021-02-02 16:48:17 +0100

    modif obstacle

--- src/main/java/com/fr/uha/ensisa/java/restapi/model/ObstacleType.java
+++ src/main/java/com/fr/uha/ensisa/java/restapi/model/ObstacleType.java
@@ -1,30 +1,8 @@
 package com.fr.uha.ensisa.java.restapi.model;
 
-public class ObstacleType {
+public enum ObstacleType {
 
-	public static final ObstacleType OTHER = new ObstacleType(0);
-    public static final ObstacleType ENIGME = new ObstacleType(1);
-    public static final ObstacleType WALK = new ObstacleType(2);
-    
-    private int value;
-
-    private ObstacleType( int value ) {
-        this.value = value;
-    }
-
-    public int toInt() {
-        return value;   
-    }
-    
-    public static ObstacleType fromInt( int value ) {
-        switch( value ) {
-            case 0: return OTHER;
-            case 1: return ENIGME;
-            default: return WALK;
-        }
-    }
-	
-	
-	
-	
+	OTHERz,
+	ENIGMA,
+	PHYSICAL
 }
